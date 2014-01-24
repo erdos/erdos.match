ab140eae57efc69301c6d83d80f387bb317c80e4
Janos Erdos <erdosj@gmail.com>
2014-01-24 22:37:35 +0100

    project.clj added

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject erdos.match "0.1"
+  :description "A small pattern matching library"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :warn-on-reflection true
+  :min-lein-version "2.0.0"
+  :url "http://github.com/erdos/erdos.match"
+  :test-paths ["test"]
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
